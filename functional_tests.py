536c20b8e1cc1e9254f7f59d60c5e876b5444ad6
Harish Narayanan <hnarayanan@gmail.com>
2016-11-24 13:10:33 +0000

    Setup a skeletal django project to pass the test (1.2)

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
+
 browser = webdriver.Firefox(firefox_binary=FirefoxBinary(
     firefox_path='/home/ubuntu/Desktop/testinggoat/software/firefox/firefox'
 ))
 browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
